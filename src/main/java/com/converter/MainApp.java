3e590b87792f9fdbc14165308955b941f29c511d
Bastion01 <vlad.kornelyuk@mail.ru>
2026-02-04 00:06:03 +0300

    java fx ui mvp added

--- src/main/java/com/converter/MainApp.java
+++ src/main/java/com/converter/MainApp.java
@@ -4,14 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//import io.github.mkpaz.atlantafx.base.theme.PrimerDark;
+import atlantafx.base.theme.PrimerDark;
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         // Устанавливаем современную темную тему
-//        Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
+        Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
 
         // Загружаем интерфейс из FXML файла
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
