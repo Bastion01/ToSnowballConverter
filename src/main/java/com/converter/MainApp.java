5515563dab9e9a2aabf332928517f41f9a5424b9
ulka1118 <uladzislau.karnialiuk@netcracker.com>
2026-02-03 21:57:36 +0300

    java fx UI attempts

---
+++ src/main/java/com/converter/MainApp.java
@@ -0,0 +1,29 @@
+package com.converter;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+//import io.github.mkpaz.atlantafx.base.theme.PrimerDark;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Устанавливаем современную темную тему
+//        Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
+
+        // Загружаем интерфейс из FXML файла
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+
+        stage.setTitle("Finstore to Snowball Converter");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
